a84697b5ba9b558cdddcb9f9adae1557cc47053f
hee100 <ggng200@naver.com>
2023-10-10 03:21:19 +0900

    뉴코드

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public class Main {
         // main 메서드 내용을 작성하세요
         System.out.println("QQQQQ");
         System.out.println("Hello, World!");
+
+        System.out.println("new code~ in A");
     }
 }
